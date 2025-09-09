e3d808e8332b78a56ed2d8de04cd5d1f9ed2592a
Luis Eduardo San Martin <lsanmartin@worldbank.org>
2025-09-09 09:58:49 -0400

    added hash-only comparison for non-tabular data

--- resources/compare_datasets/compare_datasets.R
+++ resources/compare_datasets/compare_datasets.R
@@ -31,9 +31,14 @@ read_dataset <- function(file) {
     )
 }
 
+# === LOCALS ===
+dataframe_types <- c('csv', 'xlsx', 'dta')
+
 # === LOAD FILES ===
 pkg_files <- list.files(pkg_path, full.names = TRUE)
 pub_files <- list.files(pub_path, full.names = TRUE)
+pkg_files <- pkg_files[pkg_files != 'package/README.md']
+pub_files <- pub_files[pub_files != 'public/README.md']
 
 matched_files <- intersect(
     file_path_sans_ext(basename(pkg_files)),
@@ -48,97 +53,111 @@ for (name in matched_files) {
     pkg_file <- pkg_files[basename(file_path_sans_ext(pkg_files)) == name][1]
     pub_file <- pub_files[basename(file_path_sans_ext(pub_files)) == name][1]
     
-    pkg_data <- tryCatch(read_dataset(pkg_file), error = function(e) NULL)
-    pub_data <- tryCatch(read_dataset(pub_file), error = function(e) NULL)
-    if (is.null(pkg_data) || is.null(pub_data)) next
+    # for data types
+    if (file_ext(pkg_file) %in% dataframe_types &
+        file_ext(pub_file) %in% dataframe_types) {
     
-    pkg_data <- clean_names(pkg_data)
-    pub_data <- clean_names(pub_data)
+        pkg_data <- tryCatch(read_dataset(pkg_file), error = function(e) NULL)
+        pub_data <- tryCatch(read_dataset(pub_file), error = function(e) NULL)
+        if (is.null(pkg_data) || is.null(pub_data)) next
+        
+        pkg_data <- clean_names(pkg_data)
+        pub_data <- clean_names(pub_data)
+        
+        vars_pkg <- names(pkg_data)
+        vars_pub <- names(pub_data)
+        common_vars <- intersect(vars_pkg, vars_pub)
+        if (length(common_vars) == 0) next
+        
+        missing_in_pub <- setdiff(vars_pkg, vars_pub)
+        missing_in_pkg <- setdiff(vars_pub, vars_pkg)
+        
+        vars_in_public_status <- if (length(missing_in_pub) == 0) {
+            "All public variables are in package dataset"
+        } else {
+            paste("Missing in public:", paste(missing_in_pub, collapse = "; "))
+        }
+        
+        vars_in_package_status <- if (length(missing_in_pkg) == 0) {
+            "All package variables are in public dataset"
+        } else {
+            paste("Missing in package:", paste(missing_in_pkg, collapse = "; "))
+        }
+        
+        row_match_status <- if (nrow(pkg_data) == nrow(pub_data)) {
+            "Same number of rows"
+        } else {
+            paste0("Different number of rows: package = ", nrow(pkg_data), ", public = ", nrow(pub_data))
+        }
+        
+        value_diff_vars <- c()
+        type_mismatch_vars <- c()
+        
+        for (var in common_vars) {
+            pkg_col <- pkg_data[[var]]
+            pub_col <- pub_data[[var]]
+            
+            type_pkg <- typeof(pkg_col)
+            type_pub <- typeof(pub_col)
+            type_mismatch <- !identical(type_pkg, type_pub)
+            
+            # Trim to equal length
+            n <- min(length(pkg_col), length(pub_col))
+            pkg_trimmed <- format(pkg_col[1:n])
+            pub_trimmed <- format(pub_col[1:n])
+            
+            diffs <- tibble(
+                row = 1:n,
+                package_value = pkg_trimmed,
+                public_value = pub_trimmed
+            ) %>%
+                filter(package_value != public_value & !(is.na(package_value) & is.na(public_value))) %>%
+                mutate(
+                    file = name,
+                    variable = var,
+                    type_package = type_pkg,
+                    type_public = type_pub,
+                    type_mismatch = ifelse(type_mismatch, "yes", "no"),
+                    .before = 1
+                )
+            
+            if (nrow(diffs) > 0) {
+                value_diff_vars <- c(value_diff_vars, var)
+                if (detailed_diff) {
+                    all_value_diffs[[paste(name, var, sep = "_")]] <- diffs
+                }
+            }
+            
+            if (type_mismatch) {
+                type_mismatch_vars <- c(type_mismatch_vars, paste0(var, " [", type_pkg, " vs ", type_pub, "]"))
+            }
+        }
+        
+        value_diff_summary <- if (length(value_diff_vars) > 0) {
+            paste("Different values in:", paste(value_diff_vars, collapse = "; "))
+        } else {
+            "All common variable values match"
+        }
+        
+        type_diff_summary <- if (length(type_mismatch_vars) > 0) {
+            paste("Type mismatches in:", paste(type_mismatch_vars, collapse = "; "))
+        } else {
+            "All data types match"
+        }
     
-    vars_pkg <- names(pkg_data)
-    vars_pub <- names(pub_data)
-    common_vars <- intersect(vars_pkg, vars_pub)
-    if (length(common_vars) == 0) next
-    
-    missing_in_pub <- setdiff(vars_pkg, vars_pub)
-    missing_in_pkg <- setdiff(vars_pub, vars_pkg)
-    
-    vars_in_public_status <- if (length(missing_in_pub) == 0) {
-        "All public variables are in package dataset"
+    # Non-tabular data
     } else {
-        paste("Missing in public:", paste(missing_in_pub, collapse = "; "))
-    }
-    
-    vars_in_package_status <- if (length(missing_in_pkg) == 0) {
-        "All package variables are in public dataset"
-    } else {
-        paste("Missing in package:", paste(missing_in_pkg, collapse = "; "))
-    }
-    
-    row_match_status <- if (nrow(pkg_data) == nrow(pub_data)) {
-        "Same number of rows"
-    } else {
-        paste0("Different number of rows: package = ", nrow(pkg_data), ", public = ", nrow(pub_data))
-    }
+        row_match_status       <- ''
+        vars_in_public_status  <- ''
+        vars_in_package_status <- ''
+        value_diff_summary     <- ''
+        type_diff_summary      <- ''
+    }    
     
     hash_pkg <- calculate_sha256(pkg_file)
     hash_pub <- calculate_sha256(pub_file)
     hash_status <- if (identical(hash_pkg, hash_pub)) "hashes match" else "hashes differ"
     
-    value_diff_vars <- c()
-    type_mismatch_vars <- c()
-    
-    for (var in common_vars) {
-        pkg_col <- pkg_data[[var]]
-        pub_col <- pub_data[[var]]
-        
-        type_pkg <- typeof(pkg_col)
-        type_pub <- typeof(pub_col)
-        type_mismatch <- !identical(type_pkg, type_pub)
-        
-        # Trim to equal length
-        n <- min(length(pkg_col), length(pub_col))
-        pkg_trimmed <- format(pkg_col[1:n])
-        pub_trimmed <- format(pub_col[1:n])
-        
-        diffs <- tibble(
-            row = 1:n,
-            package_value = pkg_trimmed,
-            public_value = pub_trimmed
-        ) %>%
-            filter(package_value != public_value & !(is.na(package_value) & is.na(public_value))) %>%
-            mutate(
-                file = name,
-                variable = var,
-                type_package = type_pkg,
-                type_public = type_pub,
-                type_mismatch = ifelse(type_mismatch, "yes", "no"),
-                .before = 1
-            )
-        
-        if (nrow(diffs) > 0) {
-            value_diff_vars <- c(value_diff_vars, var)
-            if (detailed_diff) {
-                all_value_diffs[[paste(name, var, sep = "_")]] <- diffs
-            }
-        }
-        
-        if (type_mismatch) {
-            type_mismatch_vars <- c(type_mismatch_vars, paste0(var, " [", type_pkg, " vs ", type_pub, "]"))
-        }
-    }
-    
-    value_diff_summary <- if (length(value_diff_vars) > 0) {
-        paste("Different values in:", paste(value_diff_vars, collapse = "; "))
-    } else {
-        "All common variable values match"
-    }
-    
-    type_diff_summary <- if (length(type_mismatch_vars) > 0) {
-        paste("Type mismatches in:", paste(type_mismatch_vars, collapse = "; "))
-    } else {
-        "All data types match"
-    }
     
     summary_report[[name]] <- tibble(
         file = name,
